865f6363400309203b2a2bfba25751d917e212d6
Tijs van Roon <t.vanroon@uva.nl>
2019-08-16 14:04:23 +0200

    WIP: init configures, read_test implemented for testing

--- src/ADS8168.cpp
+++ src/ADS8168.cpp
@@ -7,13 +7,20 @@
 #include "driver/gpio.h"
 #include "driver/spi_master.h"
 #include "freertos/task.h"
+#include <esp_log.h>
+#include <esp_heap_alloc_caps.h>
 
 #include "config.h"
+#include "log.h"
 
 #include "ADS8168_reg.h"
 
+#define LOG_LOCAL_LEVEL     ESP_LOG_VERBOSE
+
 // Helpers
-#define CMD_ADDRESS_MASK     ((1 << 11) - 1)
+#define CMD_ADDRESS_MASK    ((uint16_t)(1 << 11) - 1)
+
+#define N_CHANNELS          4
 
 ADS::ADS(spi_host_device_t host, gpio_num_t cs)
 {
@@ -21,9 +28,11 @@ ADS::ADS(spi_host_device_t host, gpio_num_t cs)
     {
         memset(&devcfg, 0, sizeof(devcfg));
         devcfg.mode = 0,                                //SPI mode 0
-        devcfg.clock_speed_hz = ADS8168_CLOCKSPEED; 
+        devcfg.clock_speed_hz = SPI_MASTER_FREQ_20M; 
         devcfg.spics_io_num = cs;                       //CS pin
-        devcfg.queue_size = 1;                          
+        devcfg.queue_size = 256;                          
+        devcfg.command_bits = 5;
+        devcfg.address_bits = 11;
         //devcfg.pre_cb = NULL;                           //Specify pre-transfer callback to handle D/C line
     };
 
@@ -34,75 +43,111 @@ ADS::ADS(spi_host_device_t host, gpio_num_t cs)
 
 esp_err_t ADS::init()
 {
-    write_cmd(ADCCMD_NOP, 0xFFFF, 0x55);
-    write_cmd(ADCCMD_WR_REG, 0xFFFF, 0x55);
-    write_cmd(ADCCMD_RD_REG, 0xFFFF, 0x55);
-    write_cmd(ADCCMD_SET_BITS, 0xFFFF, 0x55);
-    write_cmd(ADCCMD_CLR_BITS, 0x3333, 0x55);
+    gpio_set_level(PIN_ADC_ENABLE, 1);
+    spi_device_acquire_bus(_spi, portMAX_DELAY);
+    gpio_set_level(PIN_ADC_ENABLE, 0);
+    gpio_set_level(PIN_ADC_ENABLE, 1);
+
+    // write_cmd(ADCCMD_NOP, 0xFFFF, 0x01);
+    // write_cmd(ADCCMD_WR_REG, 0xFFFF, 0x55);
+    // write_cmd(ADCCMD_RD_REG, 0xFFFF, 0x55);
+    // write_cmd(ADCCMD_SET_BITS, 0xFFFF, 0x55);
+    // write_cmd(ADCCMD_CLR_BITS, 0x3333, 0x05);
+
+    // enable writing
+    write_cmd(ADCCMD_WR_REG, REG_ACCESS, REG_ACCESS_BITS);
+
+    // Powerup all except the ref/2 buffer
+    write_cmd(ADCCMD_WR_REG, REG_PD_CNTL, PD_CNTL_PD_REFby2);
+
+    // Data type: ADC value + 4-bit channel id
+    write_cmd(ADCCMD_WR_REG, REG_DATA_CNTL, DATA_CNTL_FORMAT_CHID);
+
+    // Vref = 4V096
+    write_cmd(ADCCMD_WR_REG, REG_OFST_CAL, OFST_CAL_4V096);
+
+    // Custom channel seq mode
+    write_cmd(ADCCMD_WR_REG, REG_DEVICE_CFG, DEVICE_CFG_SEQMODE_CUSTOM);
+
+    // Only use channels 0, 2, 4, 6
+    write_cmd(ADCCMD_WR_REG, REG_AUTO_SEQ_CFG1, 
+          AUTO_SEQ_CFG1_EN_AIN0 
+        | AUTO_SEQ_CFG1_EN_AIN2 
+        | AUTO_SEQ_CFG1_EN_AIN4 
+        | AUTO_SEQ_CFG1_EN_AIN6
+        );
+    
+    // Repeat sequence, loop sequence
+    //write_cmd(WR_REG, REG_AUTO_SEQ_CFG2, AUTO_SEQ_CFG2_AUTO_REPEAT);
+    //write_cmd(WR_REG, REG_CCS_SEQ_LOOP, CCS_SEQ_LOOP_EN);
+
+    // Set channel sequence indexes
+    write_cmd(ADCCMD_WR_REG, REG_CCS_CHID_IDX(0), 0);
+    write_cmd(ADCCMD_WR_REG, REG_CCS_CHID_IDX(1), 2);
+    write_cmd(ADCCMD_WR_REG, REG_CCS_CHID_IDX(2), 4);
+    write_cmd(ADCCMD_WR_REG, REG_CCS_CHID_IDX(3), 6);
+    write_cmd(ADCCMD_WR_REG, REG_CCS_END_INDEX, 4);
+
+    write_cmd(ADCCMD_WR_REG, REG_CCS_END_INDEX, 4);
+
+    gpio_set_level(PIN_ADC_ENABLE, 0);
+    gpio_set_level(PIN_ADC_ENABLE, 1);
+    spi_device_release_bus(_spi);
+    gpio_set_level(PIN_ADC_ENABLE, 0);
 
     return ESP_OK;
 }
 
 void ADS::write_cmd(const adc_cmd_t cmd, const uint16_t address, const uint8_t data)
 {
-    union {
-        struct {
-            uint16_t cmdaddress;
-            uint8_t data;
-        } p;
-        uint8_t buf[3];
-    } tmp;
-    tmp.p.cmdaddress = (cmd << 11) | ((address & CMD_ADDRESS_MASK) << 8);
-    tmp.p.data = data;
-
-    spi_transaction_t t;
-    {
-        // memset(&t, 0, sizeof(t));       //Zero out the transaction
-        t.flags = SPI_TRANS_USE_TXDATA;
-        t.cmd = 0;
-        t.addr = 0;
-        t.length=24;                    //Command is 24 bits: [cmd:5][address:11][data:8]
-        t.rxlength = 0;
-        t.user=(void*)0;                //D/C needs to be set to 0
-
-        t.tx_data[0] = 0x11;
-        t.tx_data[1] = 0x33;
-        t.tx_data[2] = 0x55;
-        t.tx_data[3] = 0xFF;
-        
-        //memcpy(t.tx_data, tmp.buf, 3); // FIXME: directly compose the message here
-        // t.tx_buffer = &(tmp.buf);       //The data is the cmd itself
-    };
-    esp_err_t ret = spi_device_polling_transmit(_spi, &t);  //Transmit!
-    assert(ret==ESP_OK);            //Should have had no issues.
-}
-
-esp_err_t ADS::read_test()
-{
-    //get_id cmd
-    write_cmd(ADCCMD_NOP, 0x0000, 0x00);
-
     spi_transaction_t t;
     {
         // memset(&t, 0, sizeof(t));       //Zero out the transaction
         t.flags = SPI_TRANS_USE_TXDATA | SPI_TRANS_USE_RXDATA;
-        t.cmd = 0;
-        t.addr = 0;
-        t.length=24;                    //Command is 24 bits: [cmd:5][address:11][data:8]
-        t.rxlength = 0;                 // 0==copy from length, Receive 24 bits: [CHID:4][SampleData:16] FIXME: only 20 bits
+        t.cmd = cmd;
+        t.addr = address;
+        t.length = 8;
+        t.rxlength = 0;
         t.user=(void*)0;                //D/C needs to be set to 0
-
-        t.tx_buffer = 0;                // NOP, addr:0, data:0  (tx_buffer = tx_data)
+        t.tx_data[0] = data;
     };
-    memset(&t, 0, sizeof(t));
-    t.length=8*3; // bits: 3 bytes
-    t.flags = SPI_TRANS_USE_RXDATA;
-    t.user = (void*)1; // D/C
+    // spi_transaction_t *rtrans;
 
+    esp_err_t ret;
+    // ret = spi_device_polling_start(_spi, &t, portMAX_DELAY);
+    // ret = spi_device_queue_trans(_spi, &t, portMAX_DELAY);
+    // ret = spi_device_polling_end(_spi, portMAX_DELAY);
+    ret = spi_device_polling_transmit(_spi, &t);  //Transmit!
+
+    assert(ret==ESP_OK);                //Should have had no issues.
+}
+
+esp_err_t ADS::read_test()
+{
+    // Send SEQ_START (resets the sequence) and then
+    // Write n-channels * 3 NOP words to receive all enabled channels:
+    //  <WR_REG, SEQ_START, 1>, n*3*<NOP> = 3 + 4x3 = 15 bytes
+    #define TOTAL_BYTES     (3 + 3*N_CHANNELS)
+
+    // since the NOP's are all zeroes, we'll write this as one command
+    static void* rxbuf = heap_caps_calloc(TOTAL_BYTES, 1, MALLOC_CAP_DMA);
+    static void* txbuf = heap_caps_calloc(TOTAL_BYTES, 1, MALLOC_CAP_DMA);
+    spi_transaction_t t;
+    {
+        memset(&t, 0, sizeof(t));       //Zero out the transaction
+        t.flags = 0;
+        t.cmd = (uint16_t) ADCCMD_WR_REG;
+        t.addr = REG_SEQ_START;
+        t.length = (TOTAL_BYTES - 2) * 8;      // each command is 3 bytes, subtract cmd+addr of this one
+        t.rxlength = 0;                       // equal to tx
+
+        t.rx_buffer = rxbuf;                // read into rxbuf
+        t.tx_buffer = txbuf;                // stat reading one further as we're writing (also 2 less)
+    };
+    gpio_set_level(PIN_ADC_ENABLE, 1);
     esp_err_t ret = spi_device_polling_transmit(_spi, &t);
+    gpio_set_level(PIN_ADC_ENABLE, 0);
     assert( ret == ESP_OK );
-
-    // uint32_t val = *(uint32_t*) &(t.rx_data);
 
     return ESP_OK;
 }
--- src/ADS8168.h
+++ src/ADS8168.h
@@ -1,10 +1,6 @@
 #ifndef __ADS8168_H
 #define __ADS8168_H
 
-#ifndef ADS8168_CLOCKSPEED
-#define ADS8168_CLOCKSPEED      20E6
-#endif
-
 #include "esp_system.h"
 #include "driver/spi_master.h"
 #include "driver/gpio.h"
@@ -17,6 +13,7 @@ class ADS
         //~ADS();
 
         esp_err_t init();
+        esp_err_t read_test();
 
     protected:
         typedef enum {
@@ -27,13 +24,14 @@ class ADS
             ADCCMD_CLR_BITS    = 0b00100
         } adc_cmd_t;
 
+        typedef struct {
+            uint8_t a, b;
+        } pietje_t;
+        
         // Private methods
         void write_cmd(const adc_cmd_t cmd, const uint16_t address, const uint8_t data);
-        esp_err_t read_test();
 
         // Variables
-        // gpio_num_t _cs;
-        // spi_host_device_t _host;
         spi_device_handle_t _spi;
     
     private:
